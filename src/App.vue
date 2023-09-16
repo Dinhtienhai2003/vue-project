59d31ca8c38a974ca0028034e43210bf93fda716
Dinhtienhai2003 <dinhtienhai2003@gmail.com>
2023-09-16 23:25:33 +0700

    buoi4

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+<script>
+  export default {
+    data(){
+      return {
+        count: 1,
+      }
+    },
+    methods: {
+      conglengmot(){
+        this.count++;
+      },
+      trulengmot(){
+        this.count--;
+      }
+    }
+  }
+</script>
+<template>
+  
+
+   <p>Biến đếm: {{ count }}</p>
+   <div></div>
+   <button @click="conglengmot()">Cộng 1</button>
+   <button @click="trulengmot()">Trừ 1</button>
+   
+</template>
+
+<style scoped>
+p{
+  font-size: 50px;
+
+}
+button{
+  font-size: 50px;
+  color: rgb(237, 9, 9);
+  margin: 20px;
+  background-color: aqua;
+}
+
+</style>
